e91800a006c1cb5995f2fecf3a1cb4dafd33e0ea
Enzojbl <102130388+Enzojbl@users.noreply.github.com>
2022-10-09 01:41:56 +0800

    Create act7.java

---
+++ Activity-7/act7.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    char operator;
+    Double num1, num2, res;
+    
+    Scanner input = new Scanner(System.in);
+    
+    do {
+    System.out.println("Calculator Menu");
+    System.out.println("Choose an operation:");
+    System.out.println("[1] Addition");
+    System.out.println("[2] Subraction");
+    System.out.println("[3] Multiplication");
+    System.out.println("[4] Division");
+    System.out.println("[5] Exponent");
+    System.out.println("[6] Modulo");
+    System.out.println("[7] Exit");
+    
+    
+    operator = input.next().charAt(0);
+
+    
+
+    switch (operator) {
+
+      case '1':
+        System.out.println("Enter the augend:");
+        num1 = input.nextDouble();
+        System.out.println("Enter the addend:");
+        num2 = input.nextDouble();
+        res = num1 + num2;
+        System.out.print("The sum of ");
+        System.out.printf(num1 + " + " + num2 + " is " + res);
+        break;
+
+
+      case '2':
+        System.out.println("Enter the subtrahend:");
+        num1 = input.nextDouble();
+        System.out.println("Enter the minuend:");
+        num2 = input.nextDouble();
+        res = num1 - num2;
+        System.out.print("The difference of ");
+        System.out.printf(num1 + " - " + num2 + " is " + res);
+        break;
+
+
+      case '3':
+        System.out.println("Enter the multiplicand:");
+        num1 = input.nextDouble();
+        System.out.println("Enter the multiplier:");
+        num2 = input.nextDouble();
+        res = num1 * num2;
+        System.out.print("The product of ");
+        System.out.printf(num1 + " * " + num2 + " is " + res);
+        break;
+
+
+      case '4':
+        System.out.println("Enter the dividend:");
+        num1 = input.nextDouble();
+        System.out.println("Enter the divisor:");
+        num2 = input.nextDouble();
+        res = num1 / num2;
+        System.out.print("The quotient of ");
+        System.out.printf(num1 + " / " + num2 + " is " + res);
+        break;
+        
+      case '5':
+        System.out.println("Enter First Number:");
+        num1 = input.nextDouble();
+        System.out.println("Enter Second Number:");
+        num2 = input.nextDouble();
+        res = Math.pow(num1, num2);
+        System.out.print("The exponent of ");
+        System.out.printf(num1 + " ^ " + num2 + " is " + res);
+        break;
+    
+      case '6':
+        System.out.println("Enter First Number:");
+        num1 = input.nextDouble();
+        System.out.println("Enter Second Number:");
+        num2 = input.nextDouble();
+        res = num1 % num2;
+        System.out.print("The remainder of ");
+        System.out.print(num1 + " % " + num2 + " is " + res);
+        break;
+        
+      case '7':
+        System.exit(0);
+        break;
+        
+      default:
+        System.out.println("Invalid operator!");
+        break;
+        
+    }
+    break;
+    } while (operator != 8);
+    input.close();
+  }
+}
